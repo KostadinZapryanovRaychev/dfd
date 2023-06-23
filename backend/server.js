fcbb686f2f710fe64a835a3f797788fa7b968f6b
unknown <k.raychev@abv.bg>
2023-06-23 04:51:31 -0700

    Database connection has been established successfully with test db

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 require("dotenv");
 const cors = require("cors");
+const db = require("./config/db");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+db.authenticate()
+  .then(() => {
+    console.log("Database connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+  });
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
