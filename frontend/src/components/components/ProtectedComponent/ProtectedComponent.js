0fe1f86512f7362c6294a91fea77050c39da4f9d
unknown <k.raychev@abv.bg>
2023-08-05 05:09:16 -0700

    adding components for register and Protected component

---
+++ frontend/src/components/components/ProtectedComponent/ProtectedComponent.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const ProtectedComponent = () => {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      axios
+        .get("/api/protected", {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((response) => setUserData(response.data))
+        .catch((error) => console.error("Error fetching protected data:", error));
+    }
+  }, []);
+
+  return (
+    <div>
+      {userData ? (
+        <div>
+          <h2>Protected Data</h2>
+          <p>User: {userData.user.email}</p>
+        </div>
+      ) : (
+        <p>Please log in to access protected data.</p>
+      )}
+    </div>
+  );
+};
+
+export default ProtectedComponent;
---
+++ frontend/src/components/components/Register/RegisterForm.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+
+const RegisterForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Send the user registration data to the backend API
+      const response = await axios.post("/api/register", formData);
+
+      // Registration successful, redirect to a new page or handle it as you need
+      history.push("/login");
+    } catch (error) {
+      console.error("Error during user registration:", error);
+      // Handle any registration error here
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name:</label>
+        <input type="text" id="lastName" name="lastName" value={formData.lastName} onChange={handleChange} required />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default RegisterForm;
