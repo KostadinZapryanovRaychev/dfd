82ac14be653e4a9964bc9da7e234942b20a60ca4
unknown <k.raychev@abv.bg>
2023-08-05 05:22:39 -0700

    added secret key

--- backend/middlewares/authenticate.js
+++ backend/middlewares/authenticate.js
@@ -1,10 +1,15 @@
 const jwt = require("jsonwebtoken");
+require("dotenv").config(); // Load environment variables from .env file
+
+// ...
+
+const secretKey = process.env.SECRET_KEY || "KAMEN";
 
 const authenticateToken = (req, res, next) => {
   const token = req.headers["authorization"]?.split(" ")[1];
   if (!token) return res.sendStatus(401);
 
-  jwt.verify(token, "your-secret-key", (err, user) => {
+  jwt.verify(token, secretKey, (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = user;
     next();
