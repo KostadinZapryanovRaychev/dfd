1d570339866befb33497ced928233992cb053262
unknown <k.raychev@abv.bg>
2023-08-06 03:02:45 -0700

    adding login method

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,3 +36,31 @@ exports.registerUser = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if the user with the given email exists
+    const existingUser = await User.findOne({ where: { email } });
+    if (!existingUser) {
+      return res.status(400).json({ message: "User with this email does not exist" });
+    }
+
+    // Check if the password is correct
+    const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ id: existingUser.id, email: existingUser.email }, secretKey, {
+      expiresIn: "1h", // Set the token expiration time as per your requirement
+    });
+
+    res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    console.error("Error during user login:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
