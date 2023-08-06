20d35928e80267da1091d625d6f04895e49f08d2
unknown <k.raychev@abv.bg>
2023-08-06 03:51:45 -0700

    implementing login and logout fn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import RegisterForm from "./components/components/Register/RegisterForm";
 import ProtectedComponent from "./components/components/ProtectedComponent/ProtectedComponent";
-import { Link, Route, Router, Routes } from "react-router-dom";
+import { Link, Route, Router, Routes, useNavigate } from "react-router-dom";
+import LoginForm from "./components/components/LoginForm/LoginForm";
 
 function App() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
   return (
     <div>
       <nav>
@@ -15,11 +22,18 @@ function App() {
           <li>
             <Link to="/protected">Protected</Link>
           </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <button onClick={handleLogout}>Logout</button>
+          </li>
         </ul>
       </nav>
 
       <Routes>
         <Route path="/register" element={<RegisterForm />} />
+        <Route path="/login" element={<LoginForm />} />
         <Route path="/protected" element={<ProtectedComponent />} />
       </Routes>
     </div>
---
+++ frontend/src/components/components/LoginForm/LoginForm.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const LoginForm = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:5000/api/login", formData);
+      const token = response.data.token;
+      localStorage.setItem("token", token);
+      navigate("/");
+    } catch (error) {
+      console.error("Error during user login:", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
