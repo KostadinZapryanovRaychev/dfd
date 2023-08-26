9cc120c0edc54cd0e3834aaf55bde51e06da8e6f
unknown <k.raychev@abv.bg>
2023-08-26 13:25:29 +0300

    cleaning unneeded stuff

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 const db = require("./config/db");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { authenticateToken } = require("./middlewares/authenticate");
 const userRoutes = require("./routes/userRoutes");
 
-const User = require("./models/UserModel");
-
 const port = process.env.SERVER_PORT;
-
-const storedItems = new Map([
-  [1, { priceCents: 1000, name: "Lea" }],
-  [2, { priceCents: 2000, name: "Miro" }],
-]);
 
 const app = express();
 
@@ -41,7 +33,6 @@ app.use("/api", userRoutes);
 app.use(authenticateToken);
 
 app.get("/api/protected", (req, res) => {
-  // You can access the authenticated user's data in req.user
   res.json({ message: "This is a protected route", user: req.user });
 });
 
