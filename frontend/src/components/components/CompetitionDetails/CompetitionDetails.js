fb4983486172661259d45ef2e8e8a0c25f3d1d95
unknown <k.raychev@abv.bg>
2024-02-13 18:38:21 +0200

    fixing FE to prevent unauthorized requests for applications for competitions relevant user is not authorized to watch

--- frontend/src/components/components/CompetitionDetails/CompetitionDetails.js
+++ frontend/src/components/components/CompetitionDetails/CompetitionDetails.js
@@ -7,6 +7,7 @@ import {
 } from "../../../services/competitionServices";
 import NonAuthenticated from "../NonAuthenticated/NonAuthenticated";
 import { useAuth } from "../../../context/AuthContext/AuthContext";
+import NonAuthorized from "../NonAuthorized/NonAuthorized";
 
 function CompetitionDetails() {
   const [applicationsForCompetition, setApplicationForCompetition] = useState([]);
@@ -14,6 +15,8 @@ function CompetitionDetails() {
   const [selectedApplication, setSelectedApplication] = useState(null);
   const { competitionId } = useParams();
   const { userId, isAdmin } = useAuth();
+
+  const targetUserId = applicationsForCompetition[0]?.user?.id.toString();
 
   useEffect(() => {
     if (competitionId) {
@@ -45,10 +48,16 @@ function CompetitionDetails() {
     }
     handleClosePopUp();
   };
-  console.log(applicationsForCompetition, "application for competition");
-
   if (!userId) {
     return <NonAuthenticated />;
+  }
+
+  if (!isAdmin && applicationsForCompetition.length > 1) {
+    return <NonAuthorized />;
+  }
+
+  if (!isAdmin && targetUserId !== userId) {
+    return <NonAuthorized />;
   }
 
   const handleDownload = async (fileName) => {
@@ -74,57 +83,65 @@ function CompetitionDetails() {
   };
 
   return (
-    <div>
-      <h1>{nameOfCompetition}</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Grade</th>
-            <th>User ID</th>
-            {isAdmin && <th>Solution URL</th>}
-            {isAdmin && <th>Applied At</th>}
-          </tr>
-        </thead>
-        <tbody>
-          {applicationsForCompetition.map((application) => (
-            <tr key={application?.id}>
-              <td>{application?.id}</td>
-              <td>{application?.grade}</td>
-              <td>{application?.user?.firstName + " " + application?.user?.lastName}</td>
-              {isAdmin && (
-                <td>
-                  <button onClick={() => handleDownload(application?.solutionUrl)}>Download Solution</button>
-                </td>
-              )}
-              {isAdmin && <td>{application?.appliedAt}</td>}
-              {isAdmin && (
-                <td>
-                  <button onClick={() => handleOpenPopUp(application)}>Оцени</button>
-                </td>
-              )}
-            </tr>
-          ))}
-        </tbody>
-        <Link to="/">Back</Link>
-      </table>
-
-      {isPopUpOpen && isAdmin && (
+    <>
+      {applicationsForCompetition.length ? (
         <div>
-          <h2>{`Оцени application с ID: ${selectedApplication?.id}`}</h2>
-          <div>
-            <label>New Grade:</label>
-            <input
-              type="number"
-              value={selectedApplication?.grade}
-              onChange={(e) => setSelectedApplication({ ...selectedApplication, grade: e.target.value })}
-            />
-          </div>
-          <button onClick={() => handleUpdateGrade(selectedApplication?.grade)}>Update Grade</button>
-          <button onClick={handleClosePopUp}>Close</button>
+          <h1>{nameOfCompetition}</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Grade</th>
+                <th>User ID</th>
+                {isAdmin && <th>Solution URL</th>}
+                {isAdmin && <th>Applied At</th>}
+              </tr>
+            </thead>
+            <tbody>
+              {applicationsForCompetition.map((application) => (
+                <tr key={application?.id}>
+                  <td>{application?.id}</td>
+                  <td>{application?.grade}</td>
+                  <td>{application?.user?.firstName + " " + application?.user?.lastName}</td>
+                  {isAdmin && (
+                    <td>
+                      <button onClick={() => handleDownload(application?.solutionUrl)}>Download Solution</button>
+                    </td>
+                  )}
+                  {isAdmin && <td>{application?.appliedAt}</td>}
+                  {isAdmin && (
+                    <td>
+                      <button onClick={() => handleOpenPopUp(application)}>Оцени</button>
+                    </td>
+                  )}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <Link to="/">Back</Link>
+          {isPopUpOpen && isAdmin && (
+            <div>
+              <h2>{`Оцени application с ID: ${selectedApplication?.id}`}</h2>
+              <div>
+                <label>New Grade:</label>
+                <input
+                  type="number"
+                  value={selectedApplication?.grade}
+                  onChange={(e) => setSelectedApplication({ ...selectedApplication, grade: e.target.value })}
+                />
+              </div>
+              <button onClick={() => handleUpdateGrade(selectedApplication?.grade)}>Update Grade</button>
+              <button onClick={handleClosePopUp}>Close</button>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div>
+          <div>There is applications for this competition</div>
+          <Link to="/">Back</Link>
         </div>
       )}
-    </div>
+    </>
   );
 }
 
