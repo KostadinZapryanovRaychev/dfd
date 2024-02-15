ddd29c197f61511b36abd0459804d4f1b30f506e
unknown <k.raychev@abv.bg>
2024-02-15 18:57:55 +0200

    removing stripe initilly in BE

--- backend/routes/stripeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const StripeController = require("../controllers/stripeController");
-
-router.post("/create-checkout-session", StripeController.createCheckoutSession);
-router.post("/create-portal-session", StripeController.createPortalSession);
-router.post("/webhook", StripeController.handleWebhookEvent);
-
-module.exports = router;
--- backend/routes/subscriptionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const SubscribeController = require("../controllers/subscribeController");
-
-router.get("/subscribe", SubscribeController.subscribe);
-router.post("/subscribe", SubscribeController.pay);
-router.post("/success", SubscribeController.success);
-router.post("/failure", SubscribeController.failure);
