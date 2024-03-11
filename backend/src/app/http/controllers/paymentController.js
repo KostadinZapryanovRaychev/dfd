b04ee9661046bbf59c3d1c5f7d89507bb3e7392e
unknown <k.raychev@abv.bg>
2024-03-11 20:45:54 +0200

    making dummy data code in order to make this work

--- backend/src/app/http/controllers/paymentController.js
+++ backend/src/app/http/controllers/paymentController.js
@@ -3,10 +3,14 @@ const errorMessages = require("../../../../constants/errors");
 
 exports.createPaymentTransaction = async (req, res) => {
   try {
-    const role = await paymentService.createPaymentTransactionForAnUser(req.body);
-    res.status(201).json({ message: "Role created successfully", role });
+    const payment = await paymentService.createPaymentTransactionForAnUser(req.body);
+
+    // TODO call Stripe endpoint when my transaction is created
+    // when stripe reply update my transaction and its status
+    // get all this to the FE
+    res.status(201).json({ message: "Transaction created successfully", payment });
   } catch (error) {
-    console.log("Error creating role:", error);
+    console.log("Error creating transaction:", error);
     res.status(400).json({ message: errorMessages.unsuccessfull });
   }
 };
